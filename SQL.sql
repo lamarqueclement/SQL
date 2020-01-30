dfe5b6470e35cd0bf136fec82f44782e490929c6
Eytuzhee <nicolasplumet.pro@gmail>
2020-01-30 14:23:52 +0100

    Question 11

--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,3 @@
-# test
-use dataia_Nancy;
-
 alter table projetA add id int not null PRIMARY KEY NOT NULL AUTO_INCREMENT;
 alter table projetB add id int not null PRIMARY KEY NOT NULL AUTO_INCREMENT;
 
