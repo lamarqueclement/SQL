5dfaba305eb2ce1b782521421f776bb141ed3a1f
Eytuzhee <nicolasplumet.pro@gmail>
2020-01-30 13:59:17 +0100

    Question 11 par Nicolas Plumet

--- SQL.sql
+++ SQL.sql
@@ -1,2 +1,11 @@
 # test
 use dataia_Nancy;
+
+alter table projetA add id int not null PRIMARY KEY NOT NULL AUTO_INCREMENT;
+alter table projetB add id int not null PRIMARY KEY NOT NULL AUTO_INCREMENT;
+
+create table projetC as
+(
+    select * from projetA
+    natural join projetB
+)
